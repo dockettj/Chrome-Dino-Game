b022765a6ec3e88827ab84107bb855f12a8a882d
Zachary Seebeck <000200029@student.baisd.net>
2019-01-03 15:49:05 -0500

    Added gavity and player jumping.

--- DinoGame/DinoGame/Form1.Designer.cs
+++ DinoGame/DinoGame/Form1.Designer.cs
@@ -28,20 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.dino = new System.Windows.Forms.PictureBox();
+            this.tickGround = new System.Windows.Forms.Timer(this.components);
+            this.tickClock = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dino)).BeginInit();
             this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.Transparent;
+            this.ground.Image = global::DinoGame.Properties.Resources.ground1;
+            this.ground.Location = new System.Drawing.Point(0, 360);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(1287, 29);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 1;
+            this.ground.TabStop = false;
+            // 
+            // dino
+            // 
+            this.dino.BackColor = System.Drawing.Color.Transparent;
+            this.dino.Image = global::DinoGame.Properties.Resources.dino_walk_1;
+            this.dino.Location = new System.Drawing.Point(52, 291);
+            this.dino.Name = "dino";
+            this.dino.Size = new System.Drawing.Size(54, 78);
+            this.dino.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dino.TabIndex = 2;
+            this.dino.TabStop = false;
+            // 
+            // tickGround
+            // 
+            this.tickGround.Enabled = true;
+            this.tickGround.Interval = 1;
+            this.tickGround.Tick += new System.EventHandler(this.Ground_Tick);
+            // 
+            // tickClock
+            // 
+            this.tickClock.Enabled = true;
+            this.tickClock.Interval = 1;
+            this.tickClock.Tick += new System.EventHandler(this.Clock_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(851, 388);
+            this.Controls.Add(this.dino);
+            this.Controls.Add(this.ground);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Chrome Dino Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ButtonPress);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dino)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox dino;
+        private System.Windows.Forms.Timer tickGround;
+        private System.Windows.Forms.Timer tickClock;
     }
 }
 
--- DinoGame/DinoGame/Form1.cs
+++ DinoGame/DinoGame/Form1.cs
@@ -12,9 +12,55 @@ namespace DinoGame
 {
     public partial class Form1 : Form
     {
+
+        double[] vel = new double[] { 0, 0 };
+        double[] pos = new double[] { 0, 0 };
+        double[] acc = new double[] { 0, 0 };
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Ground_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Tick");
+            double movement = 5;
+            ground.Location = new Point(Convert.ToInt32(ground.Location.X - movement), ground.Location.Y);
+            if (ground.Location.X <= this.Width - ground.Width) { ground.Left = 0; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pos[0] = dino.Location.X;
+            pos[1] = dino.Location.Y;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void Clock_Tick(object sender, EventArgs e)
+        {
+            if (dino.Location.Y < 290) // Our gravity engine
+            {
+                acc[1] = 0.45;
+                vel[1] += acc[1];
+                pos[1] += (vel[1] + 0.55 * acc[1]);
+
+                dino.Location = new Point(Convert.ToInt32(pos[0]), Convert.ToInt32(pos[1]));
+            }
+            else
+            {
+                dino.Location = new Point(dino.Location.X, 290);
+                vel[1] = 0.45;
+            }
+        }
+
+        private void ButtonPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) {
+                pos[1] = 150;
+                dino.Location = new Point(Convert.ToInt32(pos[0]), Convert.ToInt32(pos[1]));
+            }
         }
     }
 }
